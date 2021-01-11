2e6efd3ce68b2240e17d37bb6bee7b6bf3b63dd7
daejo <daejo@icloud.com>
2021-01-10 18:38:55 -0800

    updates detail page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
-import { useStoreContext } from "../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +15,9 @@ import Cart from '../components/Cart';
 import { idbPromise } from "../utils/helpers";
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({})
